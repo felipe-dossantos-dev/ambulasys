441eb2510e143d5564b879425b956fe5e25ff3a8
fgsantos89 <santos.flip@gmail.com>
2015-10-27 01:04:17 -0200

    adicionei um campo no faces-config

--- src/main/java/br/fipp/ambulasys/controller/LoginController.java
+++ src/main/java/br/fipp/ambulasys/controller/LoginController.java
@@ -8,9 +8,9 @@ package br.fipp.ambulasys.controller;
 import br.fipp.ambulasys.repository.Pessoas;
 import java.io.Serializable;
 import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpSession;
  * @author felipe
  */
 @Named
-@ViewScoped
+@SessionScoped
 public class LoginController implements Serializable{
     
     @EJB
@@ -52,9 +52,9 @@ public class LoginController implements Serializable{
                     new FacesMessage(FacesMessage.SEVERITY_FATAL,
                             "Usuario ou senha invalida",
                             "JOAO"));
-           return "parametros";
-        } else {
            return "login";
+        } else {
+           return "parametros";
         }
     }
     
