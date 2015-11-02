e4260b296b6dbdfc0b7955ac946321a2c970a118
fgsantos89 <santos.flip@gmail.com>
2015-11-02 20:01:18 -0200

    cadastro basico hospital

--- src/main/java/br/fipp/ambulasys/controller/HospitalController.java
+++ src/main/java/br/fipp/ambulasys/controller/HospitalController.java
@@ -5,12 +5,18 @@
  */
 package br.fipp.ambulasys.controller;
 
+import br.fipp.ambulasys.model.Cidade;
 import br.fipp.ambulasys.model.Hospital;
 import br.fipp.ambulasys.model.Pessoa;
+import br.fipp.ambulasys.model.Uf;
+import br.fipp.ambulasys.repository.Cidades;
 import br.fipp.ambulasys.repository.Hospitais;
 import br.fipp.ambulasys.repository.Pessoas;
+import br.fipp.ambulasys.repository.Ufs;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -31,14 +37,28 @@ public class HospitalController implements Serializable{
     @EJB
     private Pessoas pessoas;
     
+    @EJB
+    private Cidades cidades;
+    
+    @EJB
+    private Ufs ufs;
+    
     private Hospital hospital;
+    private Uf estado;
 
+    @PostConstruct
+    public void init() {
+        hospital = new Hospital();
+        hospital.setResponsavel(new Pessoa());
+    }
+    
     public Hospital getHospital() {
         return hospital;
     }
 
     public void setHospital(Hospital hospital) {
         this.hospital = hospital;
+        this.hospital.setDataInscricao(new Date(System.currentTimeMillis()));
     }
     
     public List<Hospital> listarHospitais() {
@@ -47,5 +67,25 @@ public class HospitalController implements Serializable{
     
     public List<Pessoa> listarPessoas() {
         return pessoas.findAll();
+    }
+    
+    public void salvar() {
+        hospitais.save(hospital);
+    }
+    
+    public List<Cidade> listarCidades() {
+        return cidades.findByEstado(estado);
+    }
+    
+    public List<Uf> listarEstados() {
+        return ufs.findAll();
+    }
+
+    public Uf getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Uf estado) {
+        this.estado = estado;
     }
 }
