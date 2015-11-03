2095861c4bce307c47c8032d705ab266d3a5d702
felipe.santos <santos.flip@gmail.com>
2015-11-03 15:02:46 -0200

    adicionado as paginas de motorista e paciente

---
+++ src/main/java/br/fipp/ambulasys/conversor/UfConversor.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.fipp.ambulasys.conversor;
+
+import br.fipp.ambulasys.model.Uf;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+/**
+ *
+ * @author felipe.santos
+ */
+@FacesConverter(forClass = Uf.class)
+public class UfConversor implements Converter{
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
